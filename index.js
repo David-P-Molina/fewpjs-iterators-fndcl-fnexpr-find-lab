c2bf94f8aa9f354e835a66d9292ff123adca4c47
David Moina <davemolina777@gmail.com>
2021-05-18 18:19:29 -0500

    commit tree 4fbcceff7344659405816fbfa3ea509ec0314b67

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+superbowlWin = (array) => {
+      let record = array.find(game => game.result === "W")
+      if (!!record) {
+        return record.year
+       } else {
+        return undefined
+        }
+  }
